a31ea9aa523852d4154790de873e85b9819f96ef
Geoteknik <dananzoff@gmail.com>
2023-03-04 19:38:15 +0700

    update navbar

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 ---
 ---
 
-{% include_relative _js/jquery.js %}  
-{% include_relative _js/bootstrap.min.js %}
+{% include_relative _js/jquery.js %}   
+{% include_relative _js/bootstrap.bundle.min.js %}
